bbf8b63b5cd29e6e63ea289a8450f1941216bf7e
Jansson-Dot <sebbebastian2@gmail.com>
2021-09-22 10:09:40 +0200

    img stack and cum

--- Ch5.py
+++ Ch5.py
@@ -3,7 +3,15 @@ import numpy as np
 
 img = cv2.imread("Resources/cards.jpg")
 
-cv2.imshow("image",img)
+width,height = 250,350
 
+pts1 = np.float32([[167,183], [306,162], [207,383], [351,354]])
+pts2 = np.float32([[0,0], [width,0], [0,height], [width,height]])
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
+
+cv2.imshow("image",img)
+cv2.imshow("Output", imgOutput)
 
 cv2.waitKey(0)
